6cd19a2516f3808dae313aacc2802bbf76fc2899
Joseph Esquivel <josephesquivel@Josephs-MacBook-Air.local>
2021-12-12 18:32:20 -0800

    done;

--- api/server.js
+++ api/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const morgan = require('morgan')
+const helmet = require('helmet')
+const userRouter = require ('./users/users-router.js');
+const { logger } = require('./middleware/middleware.js');
 
 const server = express();
+
+server.use(express.json());
 
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
+
+server.use(morgan('dev'));
+server.use(express.json());
+server.use(logger)
+server.use('/api/users', logger, userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
